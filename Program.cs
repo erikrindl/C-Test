7449e430c770f7b8a58583bc7ac813ec6789433f
Erik Rindlert <erikrindl@gmail.com>
2020-11-18 11:20:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayOfNumbers = new int[5];
+            arrayOfNumbers[0] = 13;
+            arrayOfNumbers[1] = 22;
+            arrayOfNumbers[2] = 3;
+            arrayOfNumbers[3] = 93;
+            arrayOfNumbers[4] = 1;
+
+            Console.WriteLine("Check if you picked a lucky number");
+            string nr = Console.ReadLine();
+            int myNr = Convert.ToInt32(nr);
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                if (myNr == arrayOfNumbers[i])
+                {
+                    Console.WriteLine("Winner winner chicken dinner");
+                    break;
+                } else
+                {
+                    Console.WriteLine("Not this time");
+                }
+
+            }
+            Console.WriteLine(arrayOfNumbers.Contains(3));
+        }
+    }
+}
